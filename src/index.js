7be7dbdc4f2e08ed3e6a531d096787c6d8a49bbb
RanvendraNewton <ranvendra.singh2024@nst.rishihood.edu.in>
2025-09-01 17:05:46 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,68 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { type } = require('os');
 
 const server = http.createServer((req, res) => {
   // TODO: Implement your code here
+
+  if (req.url === "/calculate") {
+
+
+    const filepath = path.join(__dirname, "..", '/inputs.txt')
+
+    fs.readFile(filepath, 'utf-8', (error, data) => {
+
+      if (error) {
+        res.writeHead(500, { "Content-type": "text/plain" })
+        res.end("Unable to write result")
+
+      }
+
+      const array = data.split("\n");
+
+      if (isNaN(Number(array[0])) || isNaN(Number(array[1]))) {
+        res.writeHead(400, { "Content-type": "text/plain" })
+        res.end("Invalid Number")
+
+      }
+
+      if (array[2] === "add") {
+        res.end(String(Number(array[0]) + Number(array[1])))
+      }
+      else if (array[2] === "subtract") {
+        res.end(String(Number(array[0]) - Number(array[1])))
+      }
+
+      else if (array[2] === "multipy") {
+        res.end(String(Number(array[0]) * Number(array[1])))
+      }
+
+      else if (array[2] === "divide") {
+
+        if (Number(array[1]) === 0) {
+          res.writeHead(400, { "Content-type": "text/plain" })
+          res.end("Division by zero")
+
+        }
+        res.end(String(Number(array[0]) / Number(array[1])))
+      }
+
+      else {
+        res.writeHead(400, { "Content-type": "text/plain" })
+        res.end("Invalid Operator")
+      }
+
+    })
+
+  }
+
+  else {
+    res.writeHead(404, { "Content-type": "text/plain" })
+    res.end("Not Found")
+
+  }
+
 });
 
 // Do not modify this
